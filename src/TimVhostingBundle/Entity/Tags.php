7d6c30375bc15fc8a9ec0a5e16f92fc3f45ce854
Alexander <tim96@yandex.ru>
2015-10-07 21:06:54 +0400

    Update "Tags".

--- src/TimVhostingBundle/Entity/Tags.php
+++ src/TimVhostingBundle/Entity/Tags.php
@@ -4,6 +4,7 @@ namespace TimVhostingBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use TimVhostingBundle\Entity\Base\BaseEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Tags
@@ -22,6 +23,26 @@ class Tags extends BaseEntity
      */
     private $id;
 
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Application\Sonata\UserBundle\Entity\User")
+     * @ORM\JoinColumn(name="author_id", referencedColumnName="id")
+     */
+    protected $author;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
     /**
      * Get id
@@ -40,7 +61,54 @@ class Tags extends BaseEntity
      */
     public function __toString()
     {
-        return $this->id ? (string)$this->id : null;
+        return $this->id ? (string)$this->name : null;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Tags
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set author
+     *
+     * @param \Application\Sonata\UserBundle\Entity\User $author
+     *
+     * @return Tags
+     */
+    public function setAuthor(\Application\Sonata\UserBundle\Entity\User $author = null)
+    {
+        $this->author = $author;
+    
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return \Application\Sonata\UserBundle\Entity\User
+     */
+    public function getAuthor()
+    {
+        return $this->author;
     }
 }
-
