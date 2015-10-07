5790e6e576249471005c7bacff9300176088f23c
Alexander <tim96@yandex.ru>
2015-10-07 21:11:51 +0400

    Add "Video" entity and repository.

---
+++ src/TimVhostingBundle/Entity/Video.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace TimVhostingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use TimVhostingBundle\Entity\Base\BaseEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Video
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="TimVhostingBundle\Entity\VideoRepository")
+ */
+class Video extends BaseEntity
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    protected $description;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * To string
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->id ? (string)$this->name : null;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Video
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Video
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
