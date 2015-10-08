15157f28c586616de8f5568418c143045e036aef
Alexander <tim96@yandex.ru>
2015-10-08 21:35:20 +0400

    Add admin for "Tags" entity.

---
+++ src/TimVhostingBundle/Admin/TagsAdmin.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace TimVhostingBundle\Admin;
+
+// use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use TimVhostingBundle\Admin\Base\BaseAdmin;
+
+class TagsAdmin extends BaseAdmin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('updatedAt')
+            ->add('createdAt')
+            ->add('isDeleted')
+            ->add('id')
+            ->add('name')
+        ;
+
+        parent::configureDatagridFilters($datagridMapper);
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('updatedAt')
+            ->add('createdAt')
+            ->add('isDeleted')
+            ->add('id')
+            ->add('name')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('updatedAt')
+            ->add('createdAt')
+            ->add('isDeleted')
+            ->add('id')
+            ->add('name')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('updatedAt')
+            ->add('createdAt')
+            ->add('isDeleted')
+            ->add('id')
+            ->add('name')
+        ;
+    }
+}
