6f96fba593f2af3c715ebef1dde9227b8c59f8a7
Alexander <tim96@yandex.ru>
2015-10-08 21:09:09 +0400

    Add routing annotation to default controller.

--- src/TimVhostingBundle/Controller/DefaultController.php
+++ src/TimVhostingBundle/Controller/DefaultController.php
@@ -3,11 +3,23 @@
 namespace TimVhostingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/name/{name}", name="Name")
+     */
     public function indexAction($name)
     {
         return $this->render('TimVhostingBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    /**
+     * @Route("/", name="Home")
+     */
+    public function frontendAction()
+    {
+        return $this->render('TimVhostingBundle:Default:frontend.html.twig', array());
     }
 }
