470926a92e81256e0800b6bb39bf39f6cb29900f
Alexander <tim96@yandex.ru>
2015-10-11 18:59:27 +0400

    Add change language mechanism.

--- src/TimVhostingBundle/Controller/DefaultController.php
+++ src/TimVhostingBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace TimVhostingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
@@ -21,5 +23,42 @@ class DefaultController extends Controller
     public function frontendAction()
     {
         return $this->render('TimVhostingBundle:Default:frontend.html.twig', array());
+    }
+
+    /**
+     * @Route("/about", name="About")
+     */
+    public function aboutAction()
+    {
+        return $this->render('TimVhostingBundle:Default:about.html.twig', array());
+    }
+
+    /**
+     * @Route("/contribute", name="Contribute")
+     */
+    public function contributeAction()
+    {
+        return $this->render('TimVhostingBundle:Default:contribute.html.twig', array());
+    }
+
+    /**
+     * @Route("/contact", name="Contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('TimVhostingBundle:Default:contact.html.twig', array());
+    }
+
+    /**
+     * @Route("/changeLanguage/{name}", name="ChangeLanguage")
+     */
+    public function changeLanguageAction($name, Request $request)
+    {
+        $locale = substr($name, 0, 2);
+        $request->getSession()->set('_locale', $locale);
+        $request->setLocale($locale);
+
+        $referer = $request->headers->get('referer');
+        return new RedirectResponse($referer);
     }
 }
