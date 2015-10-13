6be8b2c9ae2b3a94c04e284730aa3b1687ce5b7b
Alexander <tim96@yandex.ru>
2015-10-13 20:58:53 +0400

    Add new entity "Feedback".

---
+++ src/TimVhostingBundle/Entity/Feedback.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace TimVhostingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+// use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Feedback
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="TimVhostingBundle\Entity\FeedbackRepository")
+ */
+class Feedback
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email.",
+     *     checkMX = false
+     * )
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     *
+     * @ORM\Column(name="message", type="text")
+     */
+    private $message;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     **/
+    private $createdAt;
+
+    /**
+     * @ORM\Column(name="is_answered", type="boolean", options={"default": false})
+     **/
+    private $isAnswered = false;
+
+    /**
+     * @ORM\Column(name="is_deleted", type="boolean", options={"default": false})
+     **/
+    private $isDeleted = false;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime('now');
+        $this->isAnswered = false;
+        $this->isDeleted = false;
+        $this->message = null;
+        $this->email = null;
+        $this->name = null;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Feedback
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Feedback
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Feedback
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Feedback
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set isAnswered
+     *
+     * @param bool $isAnswered
+     *
+     * @return Feedback
+     */
+    public function setIsAnswered($isAnswered)
+    {
+        $this->isAnswered = $isAnswered;
+    
+        return $this;
+    }
+
+    /**
+     * Get isAnswered
+     *
+     * @return bool
+     */
+    public function getIsAnswered()
+    {
+        return $this->isAnswered;
+    }
+
+    /**
+     * Set isDeleted
+     *
+     * @param bool $isDeleted
+     *
+     * @return Feedback
+     */
+    public function setIsDeleted($isDeleted)
+    {
+        $this->isDeleted = $isDeleted;
+    
+        return $this;
+    }
+
+    /**
+     * Get isDeleted
+     *
+     * @return bool
+     */
+    public function getIsDeleted()
+    {
+        return $this->isDeleted;
+    }
+}
