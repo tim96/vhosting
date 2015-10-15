285e8435d4a01c725a90e25c20f6d34c6502e0fc
Alexander <tim96@yandex.ru>
2015-10-15 22:06:04 +0400

    Add form

--- src/TimVhostingBundle/Controller/DefaultController.php
+++ src/TimVhostingBundle/Controller/DefaultController.php
@@ -7,8 +7,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use TimVhostingBundle\Entity\Feedback;
+use TimVhostingBundle\Entity\VideoSuggest;
 use TimVhostingBundle\Form\FeedbackType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use TimVhostingBundle\Form\VideoSuggestType;
 
 class DefaultController extends Controller
 {
@@ -38,15 +40,32 @@ class DefaultController extends Controller
 
     /**
      * @Route("/contribute", name="Contribute")
+     * @Template()
+     * @param Request $request
+     * @return array
      */
-    public function contributeAction()
+    public function contributeAction(Request $request)
     {
-        return $this->render('TimVhostingBundle:Default:contribute.html.twig', array());
+        $videoSuggest = new VideoSuggest();
+        $form = $this->createForm(new VideoSuggestType(), $videoSuggest);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            return $this->redirectToRoute('Contribute');
+        }
+
+        return array(
+            'form' => $form->createView()
+        );
     }
 
     /**
      * @Route("/contact", name="Contact")
      * @Template()
+     * @param Request $request
+     * @return array|RedirectResponse
      */
     public function contactAction(Request $request)
     {
