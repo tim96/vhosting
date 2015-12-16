e3dc42c2d0a230bd33ee9a9dd0f110006bb04e64
Alexander <tim96@yandex.ru>
2015-12-16 20:53:36 +0400

    Add handler for using google api.

---
+++ src/TimVhostingBundle/Handler/GoogleApiHandler.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim
+ * Date: 12/16/2015
+ * Time: 8:51 PM
+ */
+
+namespace TimVhostingBundle\Handler;
+
+use TimConfigBundle\Handler\Base\BaseContainerEmHandler;
+
+class GoogleApiHandler extends BaseContainerEmHandler
+{
+
+}
