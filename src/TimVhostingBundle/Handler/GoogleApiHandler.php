5ad507b3afaa73a02fb13f969767da4dca2d27f5
Alexander <tim96@yandex.ru>
2015-12-20 12:52:00 +0400

    Add function to get statistics about video.

--- src/TimVhostingBundle/Handler/GoogleApiHandler.php
+++ src/TimVhostingBundle/Handler/GoogleApiHandler.php
@@ -46,8 +46,8 @@ class GoogleApiHandler extends BaseContainerEmHandler
         $youtube = new \Google_Service_YouTube($client);
 
         try {
-
-            $videoResponce = $youtube->videos->listVideos('contentDetails', array('id' => $videoId));
+            // $videoResponce = $youtube->videos->listVideos('contentDetails', array('id' => $videoId));
+            $videoResponce = $youtube->videos->listVideos('snippet,contentDetails,statistics,status', array('id' => $videoId));
             if (!$videoResponce->count()) {
                 throw new \RuntimeException('Not found video');
             }
@@ -69,9 +69,35 @@ class GoogleApiHandler extends BaseContainerEmHandler
     public function getYoutubeVideoDuration($videoId)
     {
         $data = $this->getYoutubeVideoInfo($videoId);
+        return $this->getYoutubeVideoDurationFromData($data);
+    }
+
+    /**
+     * @param $data \Google_Service_YouTube_Video
+     * @return array
+     */
+    public function getYoutubeVideoDurationFromData($data)
+    {
         /** @var \Google_Service_YouTube_VideoContentDetails $getContentDetails */
         $getContentDetails = $data->getContentDetails();
         return $this->convertYoutubeVideoDuration($getContentDetails->getDuration());
+    }
+
+    public function getYoutubeVideoStatistics($videoId)
+    {
+        $data = $this->getYoutubeVideoInfo($videoId);
+        /** @var \Google_Service_YouTube_VideoStatistics $getStatistics */
+        return $this->getYoutubeVideoStatisticsFromData($data);
+    }
+
+    /**
+     * @param $data \Google_Service_YouTube_Video
+     * @return \Google_Service_YouTube_VideoStatistics
+     */
+    public function getYoutubeVideoStatisticsFromData($data)
+    {
+        /** @var \Google_Service_YouTube_VideoStatistics $getStatistics */
+        return $data->getStatistics();
     }
 
     public function convertYoutubeVideoDuration($youtubeTime)
