028f4e6056e3c1764c57745fe758876a95091f6c
Alexander <tim96@yandex.ru>
2015-12-30 21:00:12 +0400

    Add function to search interesting videos.

--- src/TimVhostingBundle/Command/VideoSpiderCommand.php
+++ src/TimVhostingBundle/Command/VideoSpiderCommand.php
@@ -14,6 +14,7 @@ use Symfony\Component\Console\Input\InputInterface as InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use TimVhostingBundle\Entity\Video;
 
 class VideoSpiderCommand extends ContainerAwareCommand
 {
@@ -37,6 +38,7 @@ class VideoSpiderCommand extends ContainerAwareCommand
         $this
             ->setName('video:spider')
             ->setDescription('Start update video information')
+            ->addOption('searchWords', null, InputOption::VALUE_OPTIONAL, 'The search words')
             ->addArgument('isDebug', InputArgument::OPTIONAL, 'Turn on debug mode: true, false. Default - false', false)
         ;
     }
@@ -60,7 +62,55 @@ class VideoSpiderCommand extends ContainerAwareCommand
 
         $this->configureHandler();
 
+        $searchWords = $input->getOption('searchWords');
+        $this->executeCommand($searchWords);
+
         $this->logMessage("Finish execute command.");
+    }
+
+    protected function executeCommand($searchWords)
+    {
+        $serviceYoutube = $this->container->get('tim_vhosting.google_api.handler');
+
+        $part = 'id,snippet';
+        $parameters = array(
+            'maxResults' => 20, /* from 0 to 50, default: 5 */
+            'order' => 'rating', /* date, rating, relevance, title, videoCount, viewCount */
+            'q' => $searchWords,
+            'type' => 'video', /* channel, playlist, video */
+        );
+
+        $data = $serviceYoutube->getYoutubeVideos($part, $parameters);
+        $token = $data->getNextPageToken();
+        $items = $data->getItems();
+        $countRecords = 0;
+        if (count($items) > 0) {
+
+            // todo: check videoId before save
+            $em = $this->container->get('doctrine')->getManager();
+
+            /** @var \Google_Service_YouTube_SearchResult $item */
+            foreach($items as $item) {
+                $objectId = $item->getId();
+                $objectSnippet = $item->getSnippet();
+
+                $video = new Video();
+                $video->setYoutubeVideoId($objectId['videoId']);
+                $video->setDescription($objectSnippet['description']);
+                $video->setName($objectSnippet['title']);
+                $video->setChannelId($objectSnippet['channelId']);
+                $video->setPublishedAt(new \DateTime($objectSnippet['publishedAt']));
+                $video->setIsPublic(false);
+
+                // todo: add parsing tags, meta and search user
+                $em->persist($video);
+                $em->flush();
+                $countRecords++;
+            }
+        }
+
+        // todo: add work logic with nextPageToken
+        $this->logMessage("Records created: ".$countRecords);
     }
 
     public function stopCommand()
