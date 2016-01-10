e6b8e0f12c6f7ea3f53d8e88758e494f48d6784e
Alexander <tim96@yandex.ru>
2016-01-10 16:25:42 +0400

    Add 'ApiController' class.

---
+++ src/TimVhostingBundle/Api/v1/ApiController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TimVhostingBundle\Api\v1;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ApiController extends Controller
+{
+    
+}
