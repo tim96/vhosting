ec501740ad914592ae52317f8efc59c44bb7c45d
Alexander <tim96@yandex.ru>
2016-01-18 20:35:08 +0400

    Add new module for angular '7minWorkout'.

--- src/TimVhostingBundle/Resources/public/js/12MinWorkout/app.js
+++ src/TimVhostingBundle/Resources/public/js/12MinWorkout/app.js
@@ -1,1 +1,3 @@
 
+angular.module('7minWorkout', []);
+
---
+++ src/TimVhostingBundle/Resources/public/js/12MinWorkout/workout.js
@@ -0,0 +1,7 @@
+
+angular.module('7minWorkout').controller('WorkoutController',
+    // example how to declare dependencies so that DI does not break after minification.
+    ['$scope', function($scope) {
+
+    }]
+);
