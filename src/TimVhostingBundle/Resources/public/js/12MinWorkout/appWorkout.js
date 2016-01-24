c10dab2cbff6226d5002ebcf362c09a8ce56a5f8
Alexander <tim96@yandex.ru>
2016-01-24 17:45:07 +0400

    Add example of using 'ngsanitize'.

--- src/TimVhostingBundle/Resources/public/js/12MinWorkout/appWorkout.js
+++ src/TimVhostingBundle/Resources/public/js/12MinWorkout/appWorkout.js
@@ -1,5 +1,5 @@
 
-angular.module('app', ['ngRoute', '7minWorkout']).
+angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout']).
 config(function ($routeProvider, $sceDelegateProvider) {
     $routeProvider.when('/start', {
         templateUrl: '/partials/start.html'
