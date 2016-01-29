1692daef0a74ad76624fdfedc2906b6cc7538120
Alexander <tim96@yandex.ru>
2016-01-29 22:34:22 +0400

    Add keyboard key click event handler. Add modernizer to check canvas support.

--- src/TimVhostingBundle/Resources/public/js/keyboardExample.js
+++ src/TimVhostingBundle/Resources/public/js/keyboardExample.js
@@ -1,28 +1,71 @@
 window.addEventListener("load", windowLoadHandlerNew, false);
 
-// Initialize canvas and required variables
-var canvas = document.getElementById("example"),
-    ctx = canvas.getContext("2d"),
-    W = 640, // window.innerWidth,
-    H = 480, // window.innerHeight,
-    fps = 60;
-
-canvas.width = W;
-canvas.height = H;
-
-function paintCanvas() {
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, 0, W, H);
+function canvasSupport() {
+    return Modernizr.canvas;
 }
 
-function draw() {
-    paintCanvas();
+function keyboardExampleApp() {
+
+    if (!canvasSupport()) {
+        console.log('Your browser does not support HTML5 canvas.')
+        return;
+    }
+
+    // Initialize canvas and required variables
+    var canvas = document.getElementById("example"),
+        ctx = canvas.getContext("2d"),
+        W = 640, // window.innerWidth,
+        H = 480, // window.innerHeight,
+        fps = 60;
+
+    canvas.width = W;
+    canvas.height = H;
+
+    // use <canvas id='example' tabindex='1'>
+    // canvas.addEventListener('keydown', handleKeyDown, false);
+    // or
+    document.addEventListener('keydown', handleKeyDown, false);
+
+    var ball = {
+        x: 50,
+        y: 50,
+        c: "white",
+        w: 50,
+        h: 50,
+
+        draw: function() {
+            ctx.beginPath();
+            ctx.fillStyle = this.c;
+            ctx.fillRect(this.x, this.y, this.w, this.h);
+            ctx.fill();
+        }
+    };
+
+    function handleKeyDown(e) {
+        console.log('keycode: ' + e.keyCode, e);
+        return false;
+    }
+
+    function paintCanvas() {
+        ctx.fillStyle = "green";
+        ctx.fillRect(0, 0, W, H);
+    }
+
+    function draw() {
+        paintCanvas();
+
+        ball.draw();
+    }
+
+    function init() {
+        draw();
+    }
+
+    init();
 }
 
-function init() {
-    draw();
-}
+
 
 function windowLoadHandlerNew() {
-    init();
+    keyboardExampleApp();
 }
