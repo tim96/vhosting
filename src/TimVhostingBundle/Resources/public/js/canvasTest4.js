5a4de55528a44149f934a23d2e465b16bc2aa9de
Alexander <tim96@yandex.ru>
2016-02-03 21:08:40 +0400

    Add new function in 'navigationExampleApp'.

--- src/TimVhostingBundle/Resources/public/js/canvasTest4.js
+++ src/TimVhostingBundle/Resources/public/js/canvasTest4.js
@@ -6,15 +6,25 @@ function windowLoadHandlerNew() {
 
 function navigationExampleApp() {
 
-    function isCanvasSupport() {
-        return Modernizr.canvas;
-    }
+    var canvas = null;
+    var canvasObject = null;
+    // window.isDebug = false;
+    window.isDebug = true;
+    var fps = 60;
+    var stats = null;
+    var canvasId = 'example';
 
     function init() {
         if (!isCanvasSupport()) {
             console.log('Your browser does not support HTML5 canvas.')
             return;
         }
+
+        canvasObject = new CanvasObject(canvasId);
+
+        stats = initStats();
+
+        writeLog('navigation app init', 1);
     }
 
     init();
