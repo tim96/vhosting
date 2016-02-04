93a1ab2955909f9ce33dc28ca0ff158a1663d293
Alexander <tim96@yandex.ru>
2016-02-04 21:03:28 +0400

    Add start script to webgl page example.

---
+++ src/TimVhostingBundle/Resources/public/js/webglTest1.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", windowLoadHandlerNew, false);
+
+function windowLoadHandlerNew() {
+
+    var canvasId = 'example';
+    var canvasObject = new CanvasObject(canvasId);
+
+}
