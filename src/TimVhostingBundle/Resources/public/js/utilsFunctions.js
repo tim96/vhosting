dd049ad5b14a0bce6107ca686e1283ba77fe97ab
Alexander <tim96@yandex.ru>
2016-02-05 10:29:06 +0400

    Add function to check intersect between lines.

--- src/TimVhostingBundle/Resources/public/js/utilsFunctions.js
+++ src/TimVhostingBundle/Resources/public/js/utilsFunctions.js
@@ -45,3 +45,34 @@ function writeLog(text, object) {
         console.log(text, object);
     }
 }
+
+// todo: check this function
+function intersect(sx1, sy1, sx2, sy2, dx1, dy1, dx2, dy2) {
+    var sw = sx2 - sx1 + 1;
+    var sh = sy2 - sy1 + 1;
+    var dw = dx2 - dx1 + 1;
+    var dh = dy2 - dy1 + 1;
+    if (dx1 < sx1) {
+        dw += dx1 - sx1;
+        if (dw > sw) {
+            dw = sw;
+        }
+    } else {
+        var w = sw + sx1 - dx1;
+        if (dw > w) {
+            dw = w;
+        }
+    }
+    if (dy1 < sy1) {
+        dh += dy1 - sy1;
+        if (dh > sh) {
+            dh = sh;
+        }
+    } else {
+        var h = sh + sy1 - dy1;
+        if (dh > h) {
+            dh = h;
+        }
+    }
+    return ! (dw <= 0 || dh <= 0);
+}
